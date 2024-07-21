a9da3f0c13cb35208bf29814c28fe2be43f93dbc
Satish Rathod <satishrathod.dev@gmail.com>
2024-07-21 19:36:10 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "./App.css";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/HeroSection";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
